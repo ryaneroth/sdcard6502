a521190c83cd81bd98dd80b8f2c21cf657b531f2
ryaneroth <ryaneroth@me.com>
2023-09-01 13:24:59 -0700

    Update 4_readsector.s

--- src/4_readsector.s
+++ src/4_readsector.s
@@ -19,7 +19,7 @@ PORTA_OUTPUTPINS = E | RW | RS | SD_CS | SD_SCK | SD_MOSI
 zp_sd_cmd_address = $40
 
 
-  .org $e000
+  .org $a000
 
 reset:
   ldx #$ff
@@ -29,14 +29,10 @@ reset:
   sta DDRB
   lda #PORTA_OUTPUTPINS   ; Set various pins on port A to output
   sta DDRA
-
-  jsr lcd_init
-
   jsr sd_init
 
 
   ; Read a sector
-  jsr lcd_cleardisplay
   lda #'r'
   jsr print_char
   lda #'s'
@@ -63,13 +59,13 @@ reset:
 
   jsr sd_waitresult
   cmp #$00
-  beq .readsuccess
+  beq readsuccess
 
   lda #'f'
   jsr print_char
   jmp loop
 
-.readsuccess
+readsuccess:
   lda #'s'
   jsr print_char
   lda #':'
@@ -78,23 +74,23 @@ reset:
   ; wait for data
   jsr sd_waitresult
   cmp #$fe
-  beq .readgotdata
+  beq readgotdata
 
   lda #'f'
   jsr print_char
   jmp loop
 
-.readgotdata
+readgotdata:
   ; Need to read 512 bytes.  Read two at a time, 256 times.
   lda #0
   sta $00 ; counter
-.readloop
+readloop:
   jsr sd_readbyte
   sta $01 ; byte1
   jsr sd_readbyte
   sta $02 ; byte2
   dec $00 ; counter
-  bne .readloop
+  bne readloop
 
   ; End command
   lda #SD_CS | SD_MOSI
@@ -109,6 +105,7 @@ reset:
 
   ; loop forever
 loop:
+  jsr EXIT
   jmp loop
 
 
@@ -122,14 +119,14 @@ sd_init:
 
   lda #SD_CS | SD_MOSI
   ldx #160               ; toggle the clock 160 times, so 80 low-high transitions
-.preinitloop:
+preinitloop:
   eor #SD_SCK
   sta PORTA
   dex
-  bne .preinitloop
+  bne preinitloop
   
 
-.cmd0 ; GO_IDLE_STATE - resets card to idle state, and SPI mode
+cmd0: ; GO_IDLE_STATE - resets card to idle state, and SPI mode
   lda #<cmd0_bytes
   sta zp_sd_cmd_address
   lda #>cmd0_bytes
@@ -139,9 +136,9 @@ sd_init:
 
   ; Expect status response $01 (not initialized)
   cmp #$01
-  bne .initfailed
+  bne initfailed
 
-.cmd8 ; SEND_IF_COND - tell the card how we want it to operate (3.3V, etc)
+cmd8: ; SEND_IF_COND - tell the card how we want it to operate (3.3V, etc)
   lda #<cmd8_bytes
   sta zp_sd_cmd_address
   lda #>cmd8_bytes
@@ -151,7 +148,7 @@ sd_init:
 
   ; Expect status response $01 (not initialized)
   cmp #$01
-  bne .initfailed
+  bne initfailed
 
   ; Read 32-bit return value, but ignore it
   jsr sd_readbyte
@@ -159,7 +156,7 @@ sd_init:
   jsr sd_readbyte
   jsr sd_readbyte
 
-.cmd55 ; APP_CMD - required prefix for ACMD commands
+cmd55: ; APP_CMD - required prefix for ACMD commands
   lda #<cmd55_bytes
   sta zp_sd_cmd_address
   lda #>cmd55_bytes
@@ -169,9 +166,9 @@ sd_init:
 
   ; Expect status response $01 (not initialized)
   cmp #$01
-  bne .initfailed
+  bne initfailed
 
-.cmd41 ; APP_SEND_OP_COND - send operating conditions, initialize card
+cmd41: ; APP_SEND_OP_COND - send operating conditions, initialize card
   lda #<cmd41_bytes
   sta zp_sd_cmd_address
   lda #>cmd41_bytes
@@ -181,37 +178,36 @@ sd_init:
 
   ; Status response $00 means initialised
   cmp #$00
-  beq .initialized
+  beq initialized
 
   ; Otherwise expect status response $01 (not initialized)
   cmp #$01
-  bne .initfailed
+  bne initfailed
 
   ; Not initialized yet, so wait a while then try again.
   ; This retry is important, to give the card time to initialize.
   jsr delay
-  jmp .cmd55
+  jmp cmd55
 
 
-.initialized
+initialized:
   lda #'Y'
   jsr print_char
   rts
 
-.initfailed
+initfailed:
   lda #'X'
   jsr print_char
-.loop
-  jmp .loop
+  jmp loop
 
 
-cmd0_bytes
+cmd0_bytes:
   .byte $40, $00, $00, $00, $00, $95
-cmd8_bytes
+cmd8_bytes:
   .byte $48, $00, $00, $01, $aa, $87
-cmd55_bytes
+cmd55_bytes:
   .byte $77, $00, $00, $00, $00, $01
-cmd41_bytes
+cmd41_bytes:
   .byte $69, $40, $00, $00, $00, $01
 
 
@@ -221,7 +217,7 @@ sd_readbyte:
   ; capturing bits from MISO and returning them
 
   ldx #8                      ; we'll read 8 bits
-.loop:
+sdreadloop:
 
   lda #SD_MOSI                ; enable card (CS low), set MOSI (resting state), SCK low
   sta PORTA
@@ -233,16 +229,16 @@ sd_readbyte:
   and #SD_MISO
 
   clc                         ; default to clearing the bottom bit
-  beq .bitnotset              ; unless MISO was set
+  beq bitnotset              ; unless MISO was set
   sec                         ; in which case get ready to set the bottom bit
-.bitnotset:
+bitnotset:
 
   tya                         ; transfer partial result from Y
   rol                         ; rotate carry bit into read result
   tay                         ; save partial result back to Y
 
   dex                         ; decrement counter
-  bne .loop                   ; loop if we need to read more bits
+  bne sdreadloop                   ; loop if we need to read more bits
 
   rts
 
@@ -253,15 +249,15 @@ sd_writebyte:
 
   ldx #8                      ; send 8 bits
 
-.loop:
+sdwriteloop:
   asl                         ; shift next bit into carry
   tay                         ; save remaining bits for later
 
   lda #0
-  bcc .sendbit                ; if carry clear, don't set MOSI for this bit
+  bcc sendbit                ; if carry clear, don't set MOSI for this bit
   ora #SD_MOSI
 
-.sendbit:
+sendbit:
   sta PORTA                   ; set MOSI (or not) first with SCK low
   eor #SD_SCK
   sta PORTA                   ; raise SCK keeping MOSI the same, to send the bit
@@ -269,7 +265,7 @@ sd_writebyte:
   tya                         ; restore remaining bits to send
 
   dex
-  bne .loop                   ; loop if there are more bits to send
+  bne sdwriteloop                   ; loop if there are more bits to send
 
   rts
 
@@ -284,8 +280,6 @@ sd_waitresult:
 
 sd_sendcommand:
   ; Debug print which command is being executed
-  jsr lcd_cleardisplay
-
   lda #'c'
   jsr print_char
   ldx #0
@@ -327,61 +321,8 @@ sd_sendcommand:
   pla   ; restore result code
   rts
 
-
-lcd_wait:
-  pha
-  lda #%00000000  ; Port B is input
-  sta DDRB
-.busy:
-  lda #RW
-  sta PORTA
-  lda #(RW | E)
-  sta PORTA
-  lda PORTB
-  and #%10000000
-  bne .busy
-
-  lda #RW
-  sta PORTA
-  lda #%11111111  ; Port B is output
-  sta DDRB
-  pla
-  rts
-
-lcd_instruction:
-  jsr lcd_wait
-  sta PORTB
-  lda #0         ; Clear RS/RW/E bits
-  sta PORTA
-  lda #E         ; Set E bit to send instruction
-  sta PORTA
-  lda #0         ; Clear RS/RW/E bits
-  sta PORTA
-  rts
-
-
-lcd_init:
-  lda #%00111000 ; Set 8-bit mode; 2-line display; 5x8 font
-  jsr lcd_instruction
-  lda #%00001110 ; Display on; cursor on; blink off
-  jsr lcd_instruction
-  lda #%00000110 ; Increment and shift cursor; don't shift display
-  jsr lcd_instruction
-
-lcd_cleardisplay:
-  lda #%00000001 ; Clear display
-  jmp lcd_instruction
-
-
 print_char:
-  jsr lcd_wait
-  sta PORTB
-  lda #RS         ; Set RS; Clear RW/E bits
-  sta PORTA
-  lda #(RS | E)   ; Set E bit to send instruction
-  sta PORTA
-  lda #RS         ; Clear E bits
-  sta PORTA
+  jsr OUTCH
   rts
 
 print_hex:
@@ -395,29 +336,29 @@ print_hex:
 print_nybble:
   and #15
   cmp #10
-  bmi .skipletter
+  bmi skipletter
   adc #6
-.skipletter
+skipletter:
   adc #48
   jsr print_char
   rts
 
 
-delay
+delay:
   ldx #0
   ldy #0
-.loop
+delayloop:
   dey
-  bne .loop
+  bne delayloop
   dex
-  bne .loop
+  bne delayloop
   rts
 
-longdelay
+longdelay:
   jsr mediumdelay
   jsr mediumdelay
   jsr mediumdelay
-mediumdelay
+mediumdelay:
   jsr delay
   jsr delay
   jsr delay
